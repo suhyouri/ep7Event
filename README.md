8a35901b35eb637aaea5dc65abc12916a958e669
SuhYouri <suhyouri@gmail.com>
2021-04-04 09:22:12 +0100

    loading from default

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # ep7Event
+
+1. cmd line tool (https://serialport.io/docs/guide-cli/)
+$ npx @serialport/list
+/dev/tty.Bluetooth-Incoming-Port
+/dev/tty.usbmodem1421    Arduino (www.arduino.cc)
---
+++ app.js
@@ -0,0 +1,35 @@
+const SerialPort = require('serialport')
+const Readline = require('@serialport/parser-readline')
+const express = require('express')
+const app = express()
+const http = require('http').createServer(app)
+const io = require('socket.io')(http)
+const three = require('three')
+
+const port = new SerialPort('/dev/ttyAMA0', {
+  baudRate: 9600
+})
+const parser = port.pipe(new Readline({ delimiter: '\r\n' }))
+
+app.use(express.static(__dirname + "/"))
+
+app.get("/", function (req, res, next) {
+  res.sendFile(__dirname + "/index.html")
+});
+
+io.on('connection', (socket) => {
+    console.log('a user connected')
+});
+
+http.listen(3000, () => {
+  console.log('listening on *:3000');
+});
+
+port.on('open',function() {
+    console.log('port opened')
+  })
+
+parser.on('data', function(value) {
+    console.log(value)
+    io.emit("currentcolor", value);
+  })
